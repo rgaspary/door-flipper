d44eb1ef6f5fcfec84ca0e70763f7b84c6e033ff
Renzo Gaspary <me@renzogaspary.com>
2020-04-20 22:13:31 -0400

    Adding basic UI components

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+// import { render } from 'react-dom';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      garageDoorState: 'CLOSED',
+    }
+    this.OpenGarage = this.OpenGarage.bind(this);
+    this.CloseGarage = this.CloseGarage.bind(this);
+  }
+
+  OpenGarage = () => {
+    this.setState({
+      garageDoorState: 'OPEN',
+    });
+  }
+
+  CloseGarage = () => {
+    this.setState({
+      garageDoorState: 'CLOSED',
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            Door Flipper (╯°□°）╯︵ &#128682;
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.status}>
+            THE DOOR IS {this.state.garageDoorState}!
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.fixToText}>
+            <Button 
+              style={styles.button}
+              onPress={this.OpenGarage}
+              title="Open Garage"
+            />
+            <Button 
+              style={styles.button}
+              onPress={this.CloseGarage}
+              title="Close Garage"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+  },
+  container: {
+    width: '100%',
+    margin: 50,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  status: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  button: {
+    flex: 1,
+    color: "#841584",
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
